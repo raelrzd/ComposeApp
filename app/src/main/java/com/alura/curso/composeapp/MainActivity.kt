3283437388d6ef143fa69f0e2f38af4480aa0309
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-08 18:00:29 -0300

    Ajustado o deslocamento da imagem do item do produto para que afete menos outros itens caso necessite de alterações (na distancia com os textos, por exemplo). E também foram adicionadas algumas formatações para os textos.

--- app/src/main/java/com/alura/curso/composeapp/MainActivity.kt
+++ app/src/main/java/com/alura/curso/composeapp/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -23,8 +25,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alura.curso.composeapp.ui.theme.ComposeAppTheme
 import com.alura.curso.composeapp.ui.theme.Purple500
 import com.alura.curso.composeapp.ui.theme.Teal200
@@ -49,27 +55,41 @@ class MainActivity : ComponentActivity() {
 fun ProductItem() {
     Column(
         Modifier
-            .width(200.dp)
             .height(250.dp)
+            .width(200.dp)
     ) {
         Box(
             Modifier
                 .fillMaxWidth()
                 .height(100.dp)
                 .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null,
-            Modifier
-                .size(100.dp)
-                .offset(y = (-50).dp)
-                .clip(shape = CircleShape)
-                .align(Alignment.CenterHorizontally)
-
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                Modifier
+                    .size(100.dp)
+                    .offset(y = 50.dp)
+                    .clip(shape = CircleShape)
+                    .align(Alignment.BottomCenter)
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "R$ 14,99",
+                Modifier.padding(top = 8.dp),
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400)
+            )
+        }
     }
 }
 
