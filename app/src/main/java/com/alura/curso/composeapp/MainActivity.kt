e98d5a092aed3a3f66e6cfee86dd448b5fc6085f
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-08 17:34:57 -0300

    Adicionados primeiros ajustes no item do produto utilizando o Modifier. Foram ajustados o tamanho e largura dos itens (com size, e configuraçẽos de heigth e width), posicionamento (centralização e deslocamento do eixo x/y) e cores (aplicado gradiente).

--- app/src/main/java/com/alura/curso/composeapp/MainActivity.kt
+++ app/src/main/java/com/alura/curso/composeapp/MainActivity.kt
@@ -7,23 +7,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alura.curso.composeapp.ui.theme.ComposeAppTheme
+import com.alura.curso.composeapp.ui.theme.Purple500
+import com.alura.curso.composeapp.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +36,7 @@ class MainActivity : ComponentActivity() {
             ComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     ProductItem()
                 }
@@ -44,16 +47,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ProductItem() {
-    Column {
+    Column(
+        Modifier
+            .width(200.dp)
+            .height(250.dp)
+    ) {
         Box(
             Modifier
-                .width(50.dp)
-                .height(50.dp)
-                .background(color = Color.Blue)
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
         )
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
+            contentDescription = null,
+            Modifier
+                .size(100.dp)
+                .offset(y = (-50).dp)
+                .clip(shape = CircleShape)
+                .align(Alignment.CenterHorizontally)
+
         )
         Text(text = "Texto 1")
         Text(text = "Texto 2")
