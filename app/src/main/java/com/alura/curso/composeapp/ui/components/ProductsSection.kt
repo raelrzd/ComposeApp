faf0616766e3df8d0d51b2372f348b8c39fcba54
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-10 12:02:23 -0300

    Adicionado LazyRow (Lazy Layouts) para melhor performance e escalabilidade da lista de produtos em cada seção.

--- app/src/main/java/com/alura/curso/composeapp/ui/components/ProductsSection.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/components/ProductsSection.kt
@@ -1,12 +1,12 @@
 package com.alura.curso.composeapp.ui.components
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,15 +31,14 @@ fun ProductsSection(title: String, products: List<Product>) {
             fontSize = 20.sp,
             fontWeight = FontWeight(400)
         )
-        Row(
+        LazyRow(
             Modifier
                 .padding(top = 8.dp)
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState())
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
         ) {
-            products.forEach { product ->
+            items(products) { product ->
                 ProductItem(product = product)
             }
         }
