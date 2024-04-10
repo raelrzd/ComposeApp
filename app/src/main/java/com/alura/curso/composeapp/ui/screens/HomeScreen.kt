df6ddf9ee192fd9f0600c949123ac1d0cc9532ff
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-10 12:13:43 -0300

    Adicionado LazyColumn (Lazy Layouts) para melhor performance e escalabilidade das listas de seções de produtos apresentadas na Home Screen.

--- app/src/main/java/com/alura/curso/composeapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/screens/HomeScreen.kt
@@ -1,11 +1,9 @@
 package com.alura.curso.composeapp.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,20 +16,21 @@ import com.alura.curso.composeapp.ui.theme.ComposeAppTheme
 
 @Composable
 fun HomeScreen(sections: Map<String, List<Product>>) {
-    Column(
+    LazyColumn(
         Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(vertical = 16.dp)
     ) {
-        for (section in sections) {
+        sections.forEach { section ->
             val title = section.key
             val products = section.value
-            ProductsSection(
-                title = title,
-                products = products
-            )
+            item {
+                ProductsSection(
+                    title = title,
+                    products = products
+                )
+            }
         }
     }
 }
