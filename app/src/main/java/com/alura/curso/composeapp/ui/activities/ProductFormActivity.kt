0a71b12c7e8f6eb2bffbef181cc3ae318aa036a7
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-11 16:36:37 -0300

    Implementados ajustes no Design dos componentes da tela de Formulario de Produtos.

--- app/src/main/java/com/alura/curso/composeapp/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/activities/ProductFormActivity.kt
@@ -3,7 +3,12 @@ package com.alura.curso.composeapp.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +18,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alura.curso.composeapp.ui.theme.ComposeAppTheme
 
 class ProductFormActivity : ComponentActivity() {
@@ -31,22 +39,63 @@ class ProductFormActivity : ComponentActivity() {
 
 @Composable
 fun ProductFormScreen() {
-    Column {
-        Text(text = "Criando o Produto")
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Criando o Produto",
+            fontSize = 28.sp
+        )
 
         var url by remember { mutableStateOf("") }
-        TextField(value = url, onValueChange = { url = it })
+        TextField(
+            value = url,
+            onValueChange = { url = it },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Url da Imagem")
+            }
+        )
 
         var name by remember { mutableStateOf("") }
-        TextField(value = name, onValueChange = { name = it })
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nome")
+            }
+        )
 
         var price by remember { mutableStateOf("") }
-        TextField(value = price, onValueChange = { price = it })
+        TextField(
+            value = price,
+            onValueChange = { price = it },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Preço")
+            }
+        )
 
         var description by remember { mutableStateOf("") }
-        TextField(value = description, onValueChange = { description = it })
+        TextField(
+            value = description,
+            onValueChange = { description = it },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp),
+            label = {
+                Text(text = "Descrição")
+            }
+        )
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier.fillMaxWidth()
+        ) {
             Text(text = "Salvar")
         }
     }
