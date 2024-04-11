3e9368f7b54437ac6f6f0f1031b0a358c025454e
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-11 17:34:50 -0300

    Adicionado Preview da Imagem quando o campo de URL do produto não estiver vazio. Também foi adicionado scroll no formulario e ajustado o espaçamento vertical ao entrar no modo de entrada virtual.

--- app/src/main/java/com/alura/curso/composeapp/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/activities/ProductFormActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,9 +22,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.alura.curso.composeapp.R
 import com.alura.curso.composeapp.ui.model.Product
 import com.alura.curso.composeapp.ui.theme.ComposeAppTheme
 import java.math.BigDecimal
@@ -45,7 +50,9 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
@@ -54,6 +61,17 @@ fun ProductFormScreen() {
         )
 
         var url by remember { mutableStateOf("") }
+
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
+        }
+
         TextField(
             value = url,
             onValueChange = { url = it },
