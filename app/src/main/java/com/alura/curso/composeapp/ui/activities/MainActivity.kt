e6ce7580bbf2cfa4c64d2adbc15d7635d77b33ac
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-12 16:36:43 -0300

    Ajustado para que a HomeScreen volte a tela inicial do App, e implementado Scaffold no composable App para comportar melhor os componentes do Material Design e assim foi adicionado um FAB para abrir a tela de Formulario de produtos.

--- app/src/main/java/com/alura/curso/composeapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/activities/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.alura.curso.composeapp.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.alura.curso.composeapp.sampledata.sampleSections
 import com.alura.curso.composeapp.ui.screens.HomeScreen
@@ -13,16 +23,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App(
+                onClickFab = {
+                    startActivity(Intent(this, ProductFormActivity::class.java))
+                }
+            )
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(onClickFab: () -> Unit = {}) {
     ComposeAppTheme {
-        HomeScreen(sampleSections)
-//        AllProductsScreen(sampleProducts)
+        Surface {
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = { onClickFab() }) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    HomeScreen(sampleSections)
+//                    AllProductsScreen(sampleProducts)
+                }
+            }
+        }
     }
 }
 
