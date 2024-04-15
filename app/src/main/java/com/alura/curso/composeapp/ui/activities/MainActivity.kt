cc636c465f9a36c8dab0502b22f36f478793b769
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-15 15:53:52 -0300

    Tendo em vista que o código da MainActivity se tornou bastante complexo devido ao gerenciamentos de estados, foi aplicada a técnica de utilizar Composables Stateful e Stateless, assim foi implementado um composable Stateful para HomeScreen (Stateless já existe, é o composable que não mantém estados), onde será feito o gerenciamento de todos os estados necessários para em seguida chamarmos o composable Stateless da HomeScreen.

--- app/src/main/java/com/alura/curso/composeapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/activities/MainActivity.kt
@@ -13,18 +13,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.alura.curso.composeapp.dao.ProductDao
-import com.alura.curso.composeapp.sampledata.sampleCandies
-import com.alura.curso.composeapp.sampledata.sampleDrinks
-import com.alura.curso.composeapp.sampledata.sampleProducts
 import com.alura.curso.composeapp.sampledata.sampleSections
-import com.alura.curso.composeapp.ui.model.Product
 import com.alura.curso.composeapp.ui.screens.HomeScreen
 import com.alura.curso.composeapp.ui.screens.HomeScreenStateHolder
 import com.alura.curso.composeapp.ui.theme.ComposeAppTheme
@@ -42,37 +34,7 @@ class MainActivity : ComponentActivity() {
                 },
                 content = {
                     val products = dao.products()
-                    val sections = mapOf(
-                        "Todos produtos" to products,
-                        "Promoções" to sampleDrinks + sampleCandies,
-                        "Doces" to sampleCandies,
-                        "Bebidas" to sampleDrinks
-                    )
-
-                    var text by remember { mutableStateOf("") }
-
-                    fun containsInNameOrDescription() = { product: Product ->
-                        product.name.contains(text, ignoreCase = true)
-                                || product.description?.contains(text, ignoreCase = true) ?: false
-                    }
-
-                    val filterProducts = remember(products, text) {
-                        if (text.isNotBlank()) {
-                            sampleProducts.filter(containsInNameOrDescription()) +
-                                    products.filter(containsInNameOrDescription())
-                        } else emptyList()
-                    }
-
-                    val homeScreenStateHolder = remember(products, text) {
-                        HomeScreenStateHolder(
-                            sections = sections,
-                            filterProducts = filterProducts,
-                            searchText = text,
-                            onSearchChange = { text = it }
-                        )
-                    }
-
-                    HomeScreen(stateHolder = homeScreenStateHolder)
+                    HomeScreen(products = products)
 //                    AllProductsScreen(sampleProducts)
                 }
             )
--- app/src/main/java/com/alura/curso/composeapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/screens/HomeScreen.kt
@@ -10,9 +10,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alura.curso.composeapp.sampledata.sampleCandies
+import com.alura.curso.composeapp.sampledata.sampleDrinks
+import com.alura.curso.composeapp.sampledata.sampleProducts
 import com.alura.curso.composeapp.sampledata.sampleSections
 import com.alura.curso.composeapp.ui.components.CardProductItem
 import com.alura.curso.composeapp.ui.components.ProductsSection
@@ -26,11 +33,43 @@ class HomeScreenStateHolder(
     val searchText: String = "",
     val onSearchChange: (String) -> Unit = {},
 ) {
-
     fun isShowSections(): Boolean {
         return searchText.isBlank()
     }
+}
 
+@Composable
+fun HomeScreen(products: List<Product>) {
+    val sections = mapOf(
+        "Todos produtos" to products,
+        "Promoções" to sampleDrinks + sampleCandies,
+        "Doces" to sampleCandies,
+        "Bebidas" to sampleDrinks
+    )
+
+    var text by remember { mutableStateOf("") }
+
+    fun containsInNameOrDescription() = { product: Product ->
+        product.name.contains(text, ignoreCase = true)
+                || product.description?.contains(text, ignoreCase = true) ?: false
+    }
+
+    val filterProducts = remember(products, text) {
+        if (text.isNotBlank()) {
+            sampleProducts.filter(containsInNameOrDescription()) +
+                    products.filter(containsInNameOrDescription())
+        } else emptyList()
+    }
+
+    val homeScreenStateHolder = remember(products, text) {
+        HomeScreenStateHolder(
+            sections = sections,
+            filterProducts = filterProducts,
+            searchText = text,
+            onSearchChange = { text = it }
+        )
+    }
+    HomeScreen(stateHolder = homeScreenStateHolder)
 }
 
 @Composable
