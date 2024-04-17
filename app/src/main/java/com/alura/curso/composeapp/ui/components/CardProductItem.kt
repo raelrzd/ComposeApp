47b2e995f09c7d6c2646be115f482499ce992e03
raelrzd <israelrezendeluiz2903@gmail.com>
2024-04-17 11:03:36 -0300

    Ajustado a implementação do Card de produto utilizando rememberSaveable para que mantenha o estado da descrição expandida ou escondida ao realizar a rolagem.

--- app/src/main/java/com/alura/curso/composeapp/ui/components/CardProductItem.kt
+++ app/src/main/java/com/alura/curso/composeapp/ui/components/CardProductItem.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -37,7 +37,7 @@ fun CardProductItem(
     elevation: Dp = 4.dp,
     isExpanded: Boolean = false,
 ) {
-    var expanded by remember { mutableStateOf(isExpanded) }
+    var expanded by rememberSaveable { mutableStateOf(isExpanded) }
     Card(
         modifier = modifier
             .fillMaxWidth()
